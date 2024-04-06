a4e87588afa1da33affd4145fedef126b7cb59ba
Ramita <115708712+rummy0302@users.noreply.github.com>
2024-04-06 19:36:49 +0800

    Updated streamlit_gui.py

--- streamlit_gui.py
+++ streamlit_gui.py
@@ -3,34 +3,34 @@ import pandas as pd
 import numpy as np
 import pickle
 
+from utils import find_postal
+from utils import find_nearest
 
-from utils_draft import find_postal
-from utils_draft import find_nearest
+with open('randomforest.pkl', 'rb') as f:
+    models_data = pickle.load(f)
 
-
-with open('regression_models_guisl.pkl', 'rb') as f:
-    models = pickle.load(f)
-
+models = models_data['models']
+feature_names = models_data['feature_names']
 
 @st.cache_data()
 def load_data(filepath):
     return pd.read_csv(filepath)
-
-def predict_price(features):
-    prediction_gb = models['HistGradientBoostingRegressor'].predict([list(features.values())])[0]
-    prediction_dt = models['DecisionTreeRegressor'].predict([list(features.values())])[0]
-    prediction_rf = models['RandomForestRegressor'].predict([list(features.values())])[0]
     
-    st.write("Predicted Prices:")
-    st.write(f"Gradient Boosting Regressor Prediction: ${prediction_gb:.2f}")
-    st.write(f"Decision Tree Regressor Prediction: ${prediction_dt:.2f}")
-    st.write(f"Random Forest Regressor Prediction: ${prediction_rf:.2f}")
+def predict_price(features, feature_names):
+    model_rf = models['RandomForestRegressor']['model']
 
+    X_df = pd.DataFrame([features], columns=feature_names)
+
+    prediction_rf = model_rf.predict(X_df)
+
+    st.write(f"Predicted Price: ${prediction_rf[0]:.2f}")
 
 
 
 shoppingMall_coordinates = load_data('coordinates_data/shopping_mall_coordinates.csv')
 mrt_coordinates = load_data('coordinates_data/mrt_coordinates.csv')[['STN_NAME','Latitude','Longitude']]
+school_coordinates = load_data('coordinates_data/school_coordinates.csv')[['Name','LATITUDE','LONGITUDE']]
+hawker_coordinates = load_data('coordinates_data/hdb_hawker_coordinates.csv')[['address','LATITUDE','LONGITUDE']]
 
 town_categories = ['ANG MO KIO', 'BEDOK', 'BISHAN', 'BUKIT BATOK', 'BUKIT MERAH',
                    'BUKIT PANJANG', 'BUKIT TIMAH', 'CENTRAL AREA', 'CHOA CHU KANG',
@@ -52,11 +52,10 @@ town = st.selectbox('Town', town_categories, index=0)
 flat_model = st.selectbox('Flat Model', flat_model_categories, index=4)
 flat_type = st.selectbox('Flat Type', flat_type_categories, index=0)
 floor_area = st.slider("Floor Area (sqm)", 34, 280, 44)
-# storey = st.selectbox('Maximum Storey', list(map(str, range(3, 51))), index=2)
 storey = st.slider('Maximum Storey', 3,51,12)
 year = st.text_input("Current Year", '2017') 
-# lease_commence_date = st.selectbox('Lease Commencement Date', list(reversed(range(1966, 2017))), index=1)
 lease_commence_date = st.slider('Lease Commencement Date',1966,2017,1979)
+predict_button = st.button("Predict Price")
 
 
 coord = find_postal(flat_address)
@@ -68,7 +67,7 @@ try:
     flat_coord = pd.DataFrame({'address':[coord.get('results')[0].get('ADDRESS')],
                             'LATITUDE':[coord.get('results')[0].get('LATITUDE')], 
                         'LONGITUDE':[coord.get('results')[0].get('LONGITUDE')],
-                        # 'POSTAL' : [coord.get('results')[0].get('POSTAL')]
+                        'POSTAL' : [coord.get('results')[0].get('POSTAL')]
                         })
     
 except IndexError:
@@ -88,33 +87,89 @@ flat_mrt = flat_mrt.rename(columns={0: 'flat', 1: 'mrt', 2: 'mrt_dist',
                                     3: 'mrt_within_1km'}).reset_index().drop('index', axis=1)
 mrt_1km['type'] = ['MRT']*len(mrt_1km)
 
+nearest_school,school_1km = find_nearest(flat_coord, school_coordinates)
+flat_school = pd.DataFrame.from_dict(nearest_school).T
+flat_school = flat_school.rename(columns={0: 'flat', 1: 'school', 2: 'school_dist',
+                                    3: 'school_within_1km'}).reset_index().drop('index', axis=1)
+school_1km['type'] = ['School']*len(school_1km)
 
-st.subheader("Nearest MRT")
-if flat_mrt.empty:
-    st.write("No MRT station found nearby.")
-else:
-    st.write(f"Nearest MRT Station: {flat_mrt['mrt'].iloc[0]}")
-    st.write(f"Distance to MRT Station: {flat_mrt['mrt_dist'].iloc[0]} meters")
+nearest_hawker,hawker_1km = find_nearest(flat_coord, hawker_coordinates)
+flat_hawker = pd.DataFrame.from_dict(nearest_hawker).T
+flat_hawker = flat_hawker.rename(columns={0: 'flat', 1: 'hawker', 2: 'hawker_dist',
+                                    3: 'hawker_within_1km'}).reset_index().drop('index', axis=1)
+hawker_1km['type'] = ['Hawker']*len(hawker_1km)
 
-st.subheader("Nearest Shopping Mall")
-if flat_mall.empty:
-    st.write("No shopping mall found nearby.")
-else:
-    st.write(f"Nearest Shopping Mall: {flat_mall['mall'].iloc[0]}")
-    st.write(f"Distance to Shopping Mall: {flat_mall['mall_dist'].iloc[0]} meters")
 
+
+show_amenities_button = st.button("Show Nearest Amenities")
+
+if show_amenities_button:
+    st.subheader("Nearest Amenities")
+    st.text("\n") 
+
+    if flat_mrt.empty:
+        st.write("No MRT station found nearby.")
+    else:
+        mrt_distance = "Within 1 km" if flat_mrt['mrt_within_1km'].iloc[0] else "More than 1 km"
+        nearest_mrt_name = flat_mrt['mrt'].iloc[0]
+        st.write(f"MRT Station: {nearest_mrt_name.title()} ({mrt_distance})")
+
+    if flat_mall.empty:
+        st.write("No shopping mall found nearby.")
+    else:
+        mall_distance = "Within 1 km" if flat_mall['mall_within_1km'].iloc[0] else "More than 1 km"
+        st.write(f"Shopping Mall: {flat_mall['mall'].iloc[0]} ({mall_distance})")
+
+    if flat_mrt.empty:
+        st.write("No Primary School found nearby.")
+    else:
+        school_distance = "Within 1 km" if flat_school['school_within_1km'].iloc[0] else "More than 1 km"
+        st.write(f"Nearest Primary School: {flat_school['school'].iloc[0]} ({school_distance})")
+
+    if flat_mrt.empty:
+        st.write("No Hawker Centre found nearby.")
+    else:
+        hawker_distance = "Within 1 km" if flat_hawker['hawker_within_1km'].iloc[0] else "More than 1 km"
+        st.write(f"Hawker Centre: {flat_hawker['hawker'].iloc[0]} ({hawker_distance})")
 
 features = {}
+print("\n")
+print("Features")
 features['FloorAreaSqm'] = floor_area
+print("Floor area: " + str(floor_area) )
+
 features['LeaseCommenceDate'] = lease_commence_date
+print("Lease commence date: " + str(lease_commence_date) )
+
 features['EndStoreyRange'] = storey
+print("End Storey Range: " + str(storey) )
+
 features['Year'] = int(year)
-features['Latitude'] = flat_coord['LATITUDE'].iloc[0]  
+print("Year: " + str(year) )
+
+features['Latitude'] = flat_coord['LATITUDE'].iloc[0] 
+print("Latitude: " + str(flat_coord['LATITUDE'].iloc[0] ) )
+
 features['Longitude'] = flat_coord['LONGITUDE'].iloc[0] 
-# features['Postal'] = flat_coord['Postal'].iloc[0] s
+print("Longitude: " + str(flat_coord['LONGITUDE'].iloc[0] ) )
+
+
+features['Postal'] = flat_coord['POSTAL'].iloc[0] 
+print("Postal: " + str(flat_coord['POSTAL'].iloc[0] ) )
+
 
 features['DistanceFromShoppingMall'] = flat_mall['mall_dist'].iloc[0]
+print("DistanceFromShoppingMall: " + str(flat_mall['mall_dist'].iloc[0] ) )
+
 features['DistanceFromMRT'] = flat_mrt['mrt_dist'].iloc[0]
+print("DistanceDistanceFromMRTFromShoppingMall: " + str(flat_mrt['mrt_dist'].iloc[0] ) )
+
+features['DistanceFromSchool'] = flat_school['school_dist'].iloc[0]
+print("DistanceFromSchool: " + str(flat_school['school_dist'].iloc[0] ) )
+
+features['DistanceFromHawkerCentre'] = flat_hawker['hawker_dist'].iloc[0]
+print("DistanceFromHawkerCentre: " + str(flat_hawker['hawker_dist'].iloc[0] ) )
+
 
 
 for town_cat in town_categories:
@@ -128,8 +183,6 @@ for flat_type_cat in flat_type_categories:
 for flat_model_cat in flat_model_categories:
     features[f'FlatModel_{flat_model_cat}'] = 1 if flat_model == flat_model_cat else 0
 
-
-predict_button = st.button("Predict Price")
 if predict_button:
-    predict_price(features)
-
+    predict_price(features,feature_names)
+    
